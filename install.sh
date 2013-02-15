169c8a3bf341c21eaf370dc181bfff039ffd92d2
Eugene Kashida <ekashida@gmail.com>
2013-02-15 02:03:41 -0800

    dynamically install vim bundles

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 WORKING_DIR=$(pwd)
+VIM_DIR=$HOME/.vim
+VIM_BUNDLE_DIR=$VIM_DIR/bundle
 
+ln -sfv $WORKING_DIR/vimrc          ~/.vimrc
 ln -sfv $WORKING_DIR/screenrc       ~/.screenrc
 ln -sfv $WORKING_DIR/bashrc         ~/.bashrc           # non-login shells
 ln -sfv $WORKING_DIR/bashrc         ~/.bash_profile     # login shell
@@ -10,13 +13,17 @@ ln -sfv $WORKING_DIR/inputrc        ~/.inputrc
 mkdir -p ~/.ssh
 ln -sfv $WORKING_DIR/ssh_config     ~/.ssh/config
 
-# if `~/.vim/` already exists, the subsequent `ln` command ends up creating a
-# symlink to `vim/` inside `~/.vim/` instead of the `~/.vim/` symlink itself
-if [ -e ~/.vim ]; then
-    rm -rf ~/.vim
+if [ -x $(which git) ]; then
+    rm -rf $VIM_DIR
+    mkdir -p $VIM_BUNDLE_DIR
+    pushd $VIM_BUNDLE_DIR
+    git clone git@github.com:tpope/vim-pathogen.git
+    git clone git@github.com:scrooloose/nerdtree.git
+    git clone git@github.com:hallettj/jslint.vim.git
+    git clone git@github.com:tristen/vim-sparkup.git
+    git clone git@github.com:vim-scripts/mru.vim.git
+    popd
 fi
-ln -sfv $WORKING_DIR/vim/           ~/.vim
-ln -sfv $WORKING_DIR/vimrc          ~/.vimrc
 
 # init shell config
 source ~/.bashrc
