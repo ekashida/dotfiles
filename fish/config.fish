f03f3c58f0b0f1c8b5eec08ae4720d643f69a17d
Eugene Kashida <ekashida@gmail.com>
2015-10-30 01:36:54 -0700

    Fish syntax for aliasing

--- fish/config.fish
+++ fish/config.fish
@@ -6,11 +6,11 @@ fish_vi_mode
 
 set --export EDITOR vim
 
-alias cp='cp -i'
-alias ll='ls -l'
-alias ls='ls -G'
-alias mv='mv -i'
-alias rm='rm -i'
+alias cp 'cp -i'
+alias ll 'ls -l'
+alias ls 'ls -G'
+alias mv 'mv -i'
+alias rm 'rm -i'
 
-alias git-local-branch-clean='git branch --merged | grep -v master | xargs -n 1 git branch -d'
-alias git-remote-branch-clean="git branch -r --merged | grep -v master | sed 's/origin\//:/' | xargs -n 1 git push origin"
+alias git-local-branch-clean 'git branch --merged | grep -v master | xargs -n 1 git branch -d'
+alias git-remote-branch-clean "git branch -r --merged | grep -v master | sed 's/origin\//:/' | xargs -n 1 git push origin"
