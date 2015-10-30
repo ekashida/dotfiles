607827dfa49f0448cfccbdc3739d57fcea418d55
Eugene Kashida <ekashida@gmail.com>
2015-10-30 02:42:27 -0700

    Simplify string concatenation

--- install.fish
+++ install.fish
@@ -1,26 +1,26 @@
 #!/usr/bin/env fish
 
 begin
-  set -l working_dir (pwd)
-  set -l config_dir ~/.config/fish
+  set working_dir (pwd)
+  set config_dir ~/.config/fish
 
   mkdir -p $config_dir
 
-  set -l source (printf '%s/fish/config.fish' $working_dir)
-  set -l target (printf '%s/config.fish' $config_dir)
+  set source "$working_dir/fish/config.fish"
+  set target "$config_dir/config.fish"
   if not test -L $target
     ln -sv $source $target
   end
 
-  set -l source (printf '%s/fish/functions' $working_dir)
-  set -l target (printf '%s/functions' $config_dir)
+  set source "$working_dir/fish/functions"
+  set target "$config_dir/functions"
   if not test -L $target
     ln -sv $source $target
   end
 
 
   if test -x (which git)
-    set -l vim_bundle_dir (printf '%s/.vim/bundle' $HOME)
+    set vim_bundle_dir "$HOME/.vim/bundle"
     mkdir -p $vim_bundle_dir
     pushd $vim_bundle_dir
     [ ! -e ag.vim           ]; and git clone git@github.com:rking/ag.vim.git
@@ -34,7 +34,7 @@ begin
     [ ! -e vim-sparkup      ]; and git clone git@github.com:tristen/vim-sparkup.git
     popd
 
-    set -l repos_dir (printf '%s/repos' $HOME)
+    set repos_dir "$HOME/repos"
     mkdir -p $repos_dir
     pushd $repos_dir
     if not test -d n
@@ -45,8 +45,8 @@ begin
   end
 
   if test -x (which npm)
-    set -l node_modules_dir /usr/local/lib/node_modules
-    set -l modules eslint babel-eslint eslint-plugin-react
+    set node_modules_dir /usr/local/lib/node_modules
+    set modules eslint babel-eslint eslint-plugin-react
 
     for module in $modules
       set module_dir "$node_modules_dir/$module"
