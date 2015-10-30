9ee075676806cbf76bb902edd5c7c68c3d0f942a
Eugene Kashida <ekashida@gmail.com>
2015-10-30 01:34:48 -0700

    Add git aliases

--- fish/config.fish
+++ fish/config.fish
@@ -11,3 +11,6 @@ alias ll='ls -l'
 alias ls='ls -G'
 alias mv='mv -i'
 alias rm='rm -i'
+
+alias git-local-branch-clean='git branch --merged | grep -v master | xargs -n 1 git branch -d'
+alias git-remote-branch-clean="git branch -r --merged | grep -v master | sed 's/origin\//:/' | xargs -n 1 git push origin"
