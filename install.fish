c80a56d8b12df1724e39aa869c0e5b9e2c55997a
Eugene Kashida <ekashida@gmail.com>
2015-10-30 02:09:03 -0700

    All the git installs

--- install.fish
+++ install.fish
@@ -17,4 +17,30 @@ begin
   if not test -L $target
     ln -sv $source $target
   end
+
+
+  if test -x (which git)
+    set -l vim_bundle_dir (printf '%s/.vim/bundle' $HOME)
+    mkdir -p $vim_bundle_dir
+    pushd $vim_bundle_dir
+    [ ! -e ag.vim           ]; and git clone git@github.com:rking/ag.vim.git
+    [ ! -e ctrlp.vim        ]; and git clone git@github.com:ctrlpvim/ctrlp.vim.git
+    [ ! -e editorconfig-vim ]; and git clone git@github.com:editorconfig/editorconfig-vim.git
+    [ ! -e nerdtree         ]; and git clone git@github.com:scrooloose/nerdtree.git
+    [ ! -e syntastic        ]; and git clone git@github.com:scrooloose/syntastic.git
+    [ ! -e vim-fugitive     ]; and git clone git@github.com:tpope/vim-fugitive.git
+    [ ! -e vim-javascript   ]; and git clone git@github.com:pangloss/vim-javascript.git
+    [ ! -e vim-pathogen     ]; and git clone git@github.com:tpope/vim-pathogen.git
+    [ ! -e vim-sparkup      ]; and git clone git@github.com:tristen/vim-sparkup.git
+    popd
+
+    set -l repos_dir (printf '%s/repos' $HOME)
+    mkdir -p $repos_dir
+    pushd $repos_dir
+    if not test -d n
+      git clone git@github.com:tj/n.git
+      pushd n; and make install; and popd
+    end
+    popd
+  end
 end
