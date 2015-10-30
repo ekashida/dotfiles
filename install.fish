4a1b52eb8ec94904771ab24ec2396cca1071cc96
Eugene Kashida <ekashida@gmail.com>
2015-10-30 01:01:44 -0700

    not instead of !

--- install.fish
+++ install.fish
@@ -8,14 +8,14 @@ begin
 
   set -l source_config (printf '%s/fish/config.fish' $working_dir)
   set -l target_config (printf '%s/config.fish' $config_dir)
-  if test ! -L $target_config
+  if not test -L $target_config
     ln -sfv $source_config $target_config
   end
 
   set -l source_function_dir (printf '%s/fish/functions' $working_dir)
   set -l target_function_dir (printf '%s/functions' $config_dir)
   # Quick check, since the force option doesn't seem to be working for ln
-  if test ! -L $target_function_dir
+  if not test -L $target_function_dir
     ln -sv $source_function_dir $target_function_dir
   end
 end
