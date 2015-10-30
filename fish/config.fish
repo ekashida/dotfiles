f80e7f887810673b97ba43b14392d40b22c082ae
Eugene Kashida <ekashida@gmail.com>
2015-10-30 01:48:10 -0700

    Add config.fish.local localrc

--- fish/config.fish
+++ fish/config.fish
@@ -14,3 +14,9 @@ alias rm 'rm -i'
 
 alias git-local-branch-clean 'git branch --merged | grep -v master | xargs -n 1 git branch -d'
 alias git-remote-branch-clean "git branch -r --merged | grep -v master | sed 's/origin\//:/' | xargs -n 1 git push origin"
+
+function localrc
+  set -l filepath ~/.config/fish/fish.config.local
+  vim $filepath
+  source $filepath
+end
