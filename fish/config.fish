543b13685dfefd92e6d092066a05680828b37f15
Eugene Kashida <ekashida@gmail.com>
2015-10-30 10:01:46 -0700

    Organize configs into sections

--- fish/config.fish
+++ fish/config.fish
@@ -1,30 +1,36 @@
-function fish_mode_prompt
-  # Overriding this function to disable indicator for vi mode
+begin
+  function fish_mode_prompt
+    # Overriding this function to disable indicator for vi mode
+  end
+
+  fish_vi_mode
+
+  set --export EDITOR vim
 end
 
-fish_vi_mode
+begin
+  alias cp 'cp -i'
+  alias ll 'ls -l'
+  alias ls 'ls -G'
+  alias mv 'mv -i'
+  alias rm 'rm -i'
 
-set --export EDITOR vim
-
-alias cp 'cp -i'
-alias ll 'ls -l'
-alias ls 'ls -G'
-alias mv 'mv -i'
-alias rm 'rm -i'
-
-alias git-local-branch-clean 'git branch --merged | grep -v master | xargs -n 1 git branch -d'
-alias git-remote-branch-clean "git branch -r --merged | grep -v master | sed 's/origin\//:/' | xargs -n 1 git push origin"
-
-
-set localrc_path ~/.config/fish/fish.config.local
-
-function localrc
-  vim $localrc_path
-  source $localrc_path
+  alias git-local-branch-clean 'git branch --merged | grep -v master | xargs -n 1 git branch -d'
+  alias git-remote-branch-clean "git branch -r --merged | grep -v master | sed 's/origin\//:/' | xargs -n 1 git push origin"
 end
 
-function source_localrc
-  source $localrc_path
-end
 
-source_localrc
+begin
+  set localrc_path ~/.config/fish/fish.config.local
+
+  function localrc
+    vim $localrc_path
+    source $localrc_path
+  end
+
+  function source_localrc
+    source $localrc_path
+  end
+
+  source_localrc
+end
