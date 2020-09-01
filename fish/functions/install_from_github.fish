b06848ad8f1655eab90e5a7aa38d0eb5d05fdbbb
Eugene Kashida <ekashida@gmail.com>
2020-08-31 21:53:02 -0400

    chore: remove z-fish functions

--- fish/functions/install_from_github.fish
+++
@@ -1,9 +0,0 @@
-function install_from_github
-  set repos_dir "$HOME/repos"
-  mkdir -p $repos_dir
-  pushd $repos_dir
-
-  install_z_fish
-
-  popd
-end
--- fish/functions/install_z_fish.fish
+++
@@ -1,5 +0,0 @@
-function install_z_fish
-  if not test -d z-fish
-    git clone git@github.com:sjl/z-fish.git
-  end
-end
